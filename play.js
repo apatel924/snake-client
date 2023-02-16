950dc0bfb9e254e32a22bc7b8a78a56e1f8312b6
apatel924 <apatel2@ualberta.ca>
2023-02-16 01:52:20 +0000

    add user input function with setup

--- play.js
+++ play.js
@@ -1,1 +1,17 @@
 const connect = require('./client')
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+  stdin.on("data", (key) => {
+    
+  })
+  }
+
+  handleUserInput()
+  return stdin;
+};
