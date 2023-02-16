649f6dcd3fed2da9fd7c9590970b6d4f86ab23b6
apatel924 <apatel2@ualberta.ca>
2023-02-16 02:15:52 +0000

    modify functions with exports and require

--- input.js
+++ input.js
@@ -2,7 +2,7 @@ const connect = require('./client')
 
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -11,10 +11,28 @@ const setupInput = function (conn) {
 
   const handleUserInput = function () {
   stdin.on("data", (key) => {
-    
-  })
-  }
+    if (key === '\u0003') {
+      process.exit();
+    } 
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
 
+    }
+    if (key === 's') {
+      conn.write("Move: down");
+
+    }
+    if (key === 'd') {
+      conn.write("Move: right");
+    }
+    if (key === 'z') {
+      conn.write("Say: Hello!");
+    }
+  })
+}   
   handleUserInput()
   return stdin;
 };
